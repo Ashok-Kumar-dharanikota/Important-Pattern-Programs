b18370b59a615624bf2a73231dd654fc9c7dd1f0
Ashok Kumar Dharanikota <103619560+Ashok-Kumar-dharanikota@users.noreply.github.com>
2023-06-22 22:44:33 +0530

    Create Pattern_8.java

---
+++ Pattern_8.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj=new Solution();
+            obj.printTriangle(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int N) {
+        // code here
+    // This is the outer loop which will loop for the rows.
+    for (int i = 0; i < N; i++)
+    {
+        // For printing the spaces before stars in each row
+        for (int j =0; j<i; j++)
+        {
+            System.out.print(" ");
+        }
+       
+        // For printing the stars in each row
+        for(int j=0;j< 2*N -(2*i +1);j++){
+            
+            System.out.print("*");
+        }
+        
+        // As soon as the stars for each iteration are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        System.out.println();
+    }
+        
+    }
+}
